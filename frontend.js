20a93e4b4415b62607898ad4d18a3e15a57a2c77
td-mai <td.mai90@gmail.com>
2019-09-10 11:41:22 +0200

    simple websocket chat

---
+++ frontend.js
@@ -0,0 +1,114 @@
+/**
+ * http://usejsdoc.org/
+ */
+
+$(function () {
+	"use strict";
+  
+  let content = $('#content');
+  let input = $('#input');
+  let status = $('#status');
+  
+  let myColor = false;
+  
+  let myName = false;
+  
+//if user is running mozilla then use it's built-in WebSocket
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+
+  // if browser doesn't support WebSocket, just show
+  // some notification and exit
+  if (!window.WebSocket) {
+    content.html($('<p>',
+      { text:'Sorry, but your browser doesn\'t support WebSocket.'}
+    ));
+    input.hide();
+    $('span').hide();
+    return;
+  }
+  
+  let connection = new WebSocket('ws://127.0.0.1:1337');
+  connection.onopen = function () {
+    // connection is opened and ready to use
+	 // first user enters his name
+	    input.removeAttr('disabled');
+	    status.text('Choose name:')
+  };
+
+  connection.onerror = function (error) {
+    // an error occurred when sending/receiving data
+	  content.html($('<p>', {
+	      text: 'Sorry, but there\'s some problem with your '
+	         + 'connection or the server is down.'
+	    }));
+  };
+
+  // incomming message
+  connection.onmessage = function (message) {
+    // try to decode json (I assume that each message
+    // from server is json)
+    try {
+      var json = JSON.parse(message.data);
+    } catch (e) {
+      console.log('This doesn\'t look like a valid JSON: ',
+          message.data);
+      return;
+    }
+    // handle incoming message
+    
+    if (json.type === 'color') { 
+        myColor = json.data;
+        status.text(myName + ': ').css('color', myColor);
+        input.removeAttr('disabled').focus();
+        // from now user can start sending messages
+      } else if (json.type === 'history') { // entire message history
+        // insert every single message to the chat window
+        for (var i=0; i < json.data.length; i++) {
+        addMessage(json.data[i].author, json.data[i].text,
+            json.data[i].color, new Date(json.data[i].time));
+        }
+      } else if (json.type === 'message') { // it's a single message
+        // let the user write another message
+        input.removeAttr('disabled'); 
+        addMessage(json.data.author, json.data.text,
+                   json.data.color, new Date(json.data.time));
+      } else {
+        console.log('Hmm..., I\'ve never seen JSON like this:', json);
+      }
+  };
+  
+  /**
+   * Send message when user presses Enter key
+   */
+  input.keydown(function(e) {
+    if (e.keyCode === 13) {
+      var msg = $(this).val();
+      if (!msg) {
+        return;
+      }
+      // send the message as an ordinary text
+      connection.send(msg);
+      $(this).val('');
+      // disable the input field to make the user wait until server
+      // sends back response
+      input.attr('disabled', 'disabled');
+      // we know that the first message sent from a user their name
+      if (myName === false) {
+        myName = msg;
+      	}
+    }
+  });
+  
+  /**
+   * Add message to the chat window
+   */
+  function addMessage(author, message, color, dt) {
+    content.prepend('<p><span style="color:' + color + '">'
+        + author + '</span> @ ' + (dt.getHours() < 10 ? '0'
+        + dt.getHours() : dt.getHours()) + ':'
+        + (dt.getMinutes() < 10
+          ? '0' + dt.getMinutes() : dt.getMinutes())
+        + ': ' + message + '</p>');
+  }
+  
+});
---
+++ server.js
@@ -0,0 +1,103 @@
+/**
+ * 
+ */
+"use strict";
+process.title = "node-chat";
+
+const webSocketServerPort = 1337;
+const WebSocketServer = require('websocket').server;
+const http = require('http');
+
+
+let history = [];
+
+let clients = [];
+let colors = [ 'red', 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+
+function htmlEntities(str){
+	return String(str).replace('/&/g', '&amp;').replace('/</g', '&lt;'
+			).replace('/>/g', '&gt;').replace('/"/g', '&quot;');
+	
+}
+//http server
+const server = http.createServer(function(request, response) {
+  // process HTTP request. Since we're writing just WebSockets
+  // server we don't have to implement anything.
+});
+
+server.listen(webSocketServerPort, function() { 
+	console.log((new Date()) + "Server is listening on port "+ webSocketServerPort);
+});
+
+// create the websocket server
+const wsServer = new WebSocketServer({
+	  // WebSocket server is tied to a HTTP server. WebSocket
+	  // request is just an enhanced HTTP request. 
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+	console.log((new Date()) + 'Connection form origin ' + request.origin + ' .');
+	let connection = request.accept(null, request.origin);
+	
+	let index = clients.push(connection) -1;
+	
+	let username = false;
+	let userColor = false;
+	
+	console.log((new Date())+ ' Connection accepted.');
+	
+	
+	if(history.length > 0){
+		connection.sendUTF(
+				JSON.stringify({type: 'history', data: history})
+				);
+	}
+	 // This is the most important callback for us, we'll handle
+	 // all messages from users here.
+	 connection.on('message', function(message) {
+	    if (message.type === 'utf8') {
+	      // process WebSocket message
+	    	if (username === false){
+	    		username = htmlEntities(message.utf8Data);
+	    		userColor = colors.shift();
+	    		
+	    		connection.sendUTF(
+	    				JSON.stringify({type: 'color', data: userColor}));
+	    		console.log((new Date()) + ' User is known as : '+ username + ' with '+ userColor +' color.');
+	    	}else{
+	    		console.log((new Date()) + ' Received message from '+ username + ' : '+ message.utf8Data);
+	    			
+	    	}
+	    	
+	    	let obj = {
+	    			time: (new Date()).getTime(),
+	    			text: htmlEntities(message.utf8Data),
+	    			author: username,
+	    			color: userColor	
+	    	};
+	    	
+	    	history.push(obj);
+	    	history = history.slice(-100);
+	    	
+	    	let json = JSON.stringify({type: 'message', data: obj});
+	    	//broadcast message to all client
+	    	for (var i=0; i < clients.length; i++){
+	    		clients[i].sendUTF(json);
+	    	}
+	    }
+  });
+ // user disconnected
+  connection.on('close', function(connection) {
+    // close user connection
+	    if (username !== false && userColor !== false) {
+	        console.log((new Date()) + " Peer "
+	            + connection.remoteAddress + " disconnected.");
+	        // remove user from the list of connected clients
+	        clients.splice(index, 1);
+	        // push back user's color to be reused by another user
+	        colors.push(userColor);
+	      }
+  });
+});
